62b07a7188c20888761cdcab9adc9dc69b92242a
Evguenia Krylova <ekrylova@wisc.edu>
2013-07-01 17:16:23 -0500

    added tests for various availability data combinations

---
+++ spec/location_spec.rb
@@ -0,0 +1,88 @@
+require File.join(File.dirname(__FILE__), "/spec_helper" )
+module UwCatalog
+
+  describe Location do
+    def load_data_from_file(fname)
+        f = File.read("test/data/#{fname}")
+        availability_data = Marshal::load(f)
+    end
+
+    it "non-circulating holding should have no items" do
+      availability_data = load_data_from_file('non_circulating.dat')
+      availability_data[0].holdings[0].items.size.should  == 0
+      availability_data[0].holdings[0].get_items_display.size.should  == 0
+    end
+
+    it "dvd holding should have no items" do
+      availability_data = load_data_from_file('dvd.dat')
+      availability_data[0].holdings[0].items.size.should  == 1
+      availability_data[0].holdings[0].get_items_display[:status].size.should  == 1
+    end
+
+
+    it "two volume holding with one volume renewed should have due date of that item" do
+      availability_data = load_data_from_file('two_volume_holding_with_one_volume_renewed.dat')
+      availability_data.size.should  == 1
+      availability_data[0].holdings.size.should  == 1
+      availability_data[0].holdings[0].items.size.should  == 2
+      items_status_data = availability_data[0].holdings[0].get_items_display[:status]
+      items_status_data.size.should  == 2
+      items_status_data[0][:available].should == true
+      items_status_data[1][:available].should == false
+      items_status_data[1][:status_text].should == '2 Not Available - Checked Out, Due on December 30, 2013'
+    end
+
+    it "lost holding should have item status date of the item" do
+      availability_data = load_data_from_file('lost.dat')
+      availability_data.size.should  == 1
+      availability_data[0].holdings.size.should  == 1
+      availability_data[0].holdings[0].items.size.should  == 1 
+      items_status_data = availability_data[0].holdings[0].get_items_display[:status]
+      items_status_data.size.should  == 1
+      items_status_data[0][:available].should == false
+      items_status_data[0][:status_text].should == 'Lost June 14, 2004.'
+    end
+
+    it "lost holding should have item status date of the item" do
+      availability_data = load_data_from_file('overdue.dat')
+      availability_data.size.should  == 1
+      availability_data[0].holdings.size.should  == 1
+      availability_data[0].holdings[0].items.size.should  == 1
+      items_status_data = availability_data[0].holdings[0].get_items_display[:status]
+      items_status_data.size.should  == 1
+      items_status_data[0][:available].should == false
+      items_status_data[0][:status_text].should == 'Overdue July 01, 2013.'
+    end
+
+
+    it "lost holding should have item status date of the item" do
+      availability_data = load_data_from_file('two_copies_checkedout.dat')
+      availability_data.size.should  == 1
+      availability_data[0].holdings.size.should  == 1
+      availability_data[0].holdings[0].items.size.should  == 2
+      items_status_data = availability_data[0].holdings[0].get_items_display[:status]
+      items_status_data.size.should  == 2
+      items_status_data[0][:available].should == false
+      items_status_data[0][:status_text].should == 'v.1 Not Available - Checked Out, Due on December 30, 2013'
+      items_status_data[1][:available].should == false
+      items_status_data[1][:status_text].should == 'v.2 Not Available - Checked Out, Due on December 30, 2013'
+    end
+
+    it "lost holding should have item status date of the item" do
+      availability_data = load_data_from_file('5_out_of_10_bindery.dat')
+      availability_data.size.should  == 1
+      availability_data[0].holdings.size.should  == 1
+      availability_data[0].holdings[0].items.size.should  == 10
+      items_status_data = availability_data[0].holdings[0].get_items_display[:status]
+      items_status_data.size.should  == 10
+      concise = true
+      unavailable_items = availability_data[0].holdings[0].get_items_display(concise)[:status]
+      unavailable_items.size.should  == 5
+      unavailable_items[0][:available] == false
+      unavailable_items[0][:status_text].should == 'v.1:supp. Not Available - At Bindery June 02, 2011'
+      unavailable_items[4][:available] == false
+      unavailable_items[4][:status_text].should == 'v.5:supp. Not Available - At Bindery June 02, 2011'
+    end
+
+  end
+end
